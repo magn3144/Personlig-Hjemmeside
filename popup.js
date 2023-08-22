0c528efccfa420aa739aedaeb31a81fd6133f411
magn3144 <magnus@elgaard.com>
2023-08-22 09:57:00 +0200

    Popup image animation fixed with javascript

--- popup.js
+++ popup.js
@@ -1,13 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
 	let popupOverlay = document.querySelector('.popup-overlay');
 	let popupContent = document.querySelector('.popup-content');
-	let triggerImages = document.querySelectorAll('.trigger-image');
+	let gridItems = document.querySelectorAll('.grid-item');
 	let otherElements = document.querySelectorAll(':not(.popup-overlay)');
 
 	popupOverlay.style.visibility = 'hidden';
 
-	triggerImages.forEach(function (image) {
-		image.addEventListener('click', function (event) {
+	// Get the html code into a dictionary
+	const dictionary = {};
+	fetch('./popup_html.txt')
+		.then(response => response.text())
+		.then(data => {
+			projectList = data.split('\r\n\r\n');
+			for (let i = 0; i < projectList.length; i++) {
+				let lines = projectList[i].split('\r\n');
+				let name = lines[0];
+				let html = lines.slice(1).join('\r\n');
+				console.log(name);
+				console.log(html);
+				dictionary[name] = html;
+			}
+			console.log(projectList);
+		})
+		.catch(error => {
+			console.log('Error:', error);
+		});
+	
+	// Add event listeners to all buttons, to open and change html of the popup when clicked
+	for (let i = 0; i < gridItems.length; i++) {
+		gridItems[i].addEventListener('click', function () {
 			// Make sure popup doesnt close instantly when opened
 			event.stopPropagation();
 
@@ -20,14 +41,27 @@ document.addEventListener('DOMContentLoaded', function () {
 			});
 
 			// Disable scrolling
-			document.body.style.overflow = 'hidden';
+			// document.body.style.overflow = 'hidden';
+
+			// Reset scroll position of popup when opened
+			popupContent.scrollTop = 0;
 
 			// Change id to "popup-open" when the popup is opened
 			popupContent.id = 'popup-content-open';
 			popupOverlay.id = 'popup-overlay-open';
-		});
-	});
 
+			// Change the html of the popup
+			let name = gridItems[i].id;
+			popupContent.innerHTML = dictionary[name];
+			const imageDiv = document.getElementById('popup-image-div-closed');
+			waitForNextFrame(function() {
+				imageDiv.id = 'popup-image-div-open';
+			});
+			// imageDiv.style.height = '100%';
+		});
+	};
+
+	// Close the popup when clicked outside
 	function closePopupOutside(event) {
 		if (event.target === popupOverlay && !popupContent.contains(event.target)) {
 			// popupOverlay.style.display = 'none';
@@ -51,38 +85,46 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
+	function waitForNextFrame(callback) {
+		requestAnimationFrame(function() {
+			requestAnimationFrame(callback);
+		});
+	}
+
 	// Set the html of the popup, when clicked
 
 	// Get the buttons and popup content
-	const button_list = document.getElementsByClassName('grid-item');
-	const popup_content = document.getElementById('popup-content-closed');
+	// const button_list = document.getElementsByClassName('grid-item');
+	// const popup_content = document.getElementById('popup-content-closed');
 
-	// Get the html code into a dictionary
-	const dictionary = {};
-	fetch('./popup_html.txt')
-		.then(response => response.text())
-		.then(data => {
-			projectList = data.split('\r\n\r\n');
-			for (let i = 0; i < projectList.length; i++) {
-				let lines = projectList[i].split('\r\n');
-				let name = lines[0];
-				let html = lines.slice(1).join('\r\n');
-				console.log(name);
-				console.log(html);
-				dictionary[name] = html;
-			}
-			console.log(projectList);
-		})
-		.catch(error => {
-			console.log('Error:', error);
-		});
+	// // Get the html code into a dictionary
+	// const dictionary = {};
+	// fetch('./popup_html.txt')
+	// 	.then(response => response.text())
+	// 	.then(data => {
+	// 		projectList = data.split('\r\n\r\n');
+	// 		for (let i = 0; i < projectList.length; i++) {
+	// 			let lines = projectList[i].split('\r\n');
+	// 			let name = lines[0];
+	// 			let html = lines.slice(1).join('\r\n');
+	// 			console.log(name);
+	// 			console.log(html);
+	// 			dictionary[name] = html;
+	// 		}
+	// 		console.log(projectList);
+	// 	})
+	// 	.catch(error => {
+	// 		console.log('Error:', error);
+	// 	});
 
-	// Add event listeners to all buttons, to change the popup content when clicked
-	for (let i = 0; i < button_list.length; i++) {
-		button_list[i].addEventListener('click', function () {
-			// Name of button is the id of the button
-			let name = button_list[i].id;
-			popup_content.innerHTML = dictionary[name];
-		});
-	}
+	// // Add event listeners to all buttons, to change the popup content when clicked
+	// for (let i = 0; i < button_list.length; i++) {
+	// 	button_list[i].addEventListener('click', function () {
+	// 		// Name of button is the id of the button
+	// 		let name = button_list[i].id;
+	// 		popup_content.innerHTML = dictionary[name];
+	// 		const image_div = document.getElementById('popup-image-div');
+	// 		image_div.style.height = '100%';
+	// 	});
+	// }
 });
